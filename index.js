2c0dc05ce2bb3d43933e2541d27a86ea047fb43c
Romy Maghsoudi <romy_mag@outlook.com>
2019-01-31 21:43:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
-  var position = katzDeliLine.length + 1
+  var position = katzDeliLine.length
 return "Welcome, " + name +". You are number "+ position +" in line."
 
  }
